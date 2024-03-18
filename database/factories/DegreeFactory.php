2899a6dd172c5a4e9f07f172fa4d9e58a0539e00
Dan Bracey <penguingeek1@gmail.com>
2024-03-18 21:32:21 +0000

    Add model factory

---
+++ database/factories/DegreeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class DegreeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => Str::random(1) . rand(0,9) . Str::random(1) . rand(0,9),
+            'friendly_name' => Str::random(10)
+        ];
+    }
+}
