e444786fdab2fb2b56b4c831969a9ae93c814418
Dan Bracey <penguingeek1@gmail.com>
2024-03-24 13:33:56 +0000

    Create web route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/{student}', function () {
+    return view('timetable');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
