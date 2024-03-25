58eb2f5160f83225903238687d3a8a2e145b153a
Dan Bracey <penguingeek1@gmail.com>
2024-03-25 11:16:32 +0000

    Create feature tests for Modules

---
+++ tests/Feature/ModuleTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Feature;
+
+use App\Models\Module;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use JsonException;
+use Tests\TestCase;
+
+class ModuleTest extends TestCase
+{
+    private User $user;
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutVite();
+
+        //Set up an authenticated user
+        $this->user = User::factory()->create();
+    }
+
+    /**
+     * Test that the student index page cannot be rendered by guests
+     * @return void
+     */
+    public function test_modules_page_cannot_be_accessed_by_guests(): void
+    {
+        $response = $this->get(route('module.index'));
+        $response->assertRedirect();
+    }
+
+    /**
+     * Test that the student index page can be rendered by staff
+     * @return void
+     */
+    public function test_modules_page_can_be_accessed_by_staff(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('module.index'));
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test student creation page can be rendered with form components
+     * @return void
+     */
+    public function test_module_creation_page_can_be_rendered(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('module.create'));
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test student creation page can be created
+     * @return void
+     */
+    public function test_module_can_be_created(): void
+    {
+        $response = $this->actingAs($this->user)->post(route('module.store'), [
+            'id' => 'CIS' . rand(0000, 3999),
+            'friendly_name' => 'Example Module',
+            'academic_year' => '23/24',
+        ]);
+
+        $response->assertRedirect();
+    }
+
+    public function test_module_can_be_read(): void
+    {
+        $module = Module::factory()->createOne();
+        $response = $this->actingAs($this->user)->get(route('module.show', $module->id));
+        $response->assertStatus(200);
+    }
+
+    public function test_module_edit_screen_can_be_rendered_by_staff(): void
+    {
+        $module = Module::factory()->createOne();
+
+        $response = $this->actingAs($this->user)->get(route('module.edit', $module->__get('id')));
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function test_module_can_be_updated(): void
+    {
+        // Create a random module
+        $module = Module::factory()->create();
+
+        // Update the student
+        $response = $this->actingAs($this->user)->put('/student/' . $module->__get('id'), [
+            'id' => $module->__get('id'),
+            'friendly_name' => 'Different Module',
+        ]);
+
+        // Assert the response
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        // Refresh the student model instance to get updated data from the database
+        $module->refresh();
+
+        // Assert against the updated values
+        $this->assertSame('Different Module', $module->__get('friendly_name'));
+    }
+
+    public function test_module_can_be_deleted(): void
+    {
+        $module = Module::factory()->createOne();
+        $response = $this->actingAs($this->user)->delete(route('student.destroy', $module->__get('id')));
+        $this->assertDatabaseMissing('students', [
+            'id' => $module->__get('id'),
+        ]);
+
+        $response->assertRedirect();
+    }
+}
