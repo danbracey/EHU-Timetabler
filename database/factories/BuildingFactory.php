b02f2bdf1c538c2f58db507839300a8be30cab19
Dan Bracey <penguingeek1@gmail.com>
2024-03-27 09:56:30 +0000

    Create Building factory

---
+++ database/factories/BuildingFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Building>
+ */
+class BuildingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'friendly_name' => Str::random(10)
+        ];
+    }
+}
