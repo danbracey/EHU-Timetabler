f1b7469fc4a53dc51f7d0ad1668028f99e19f746
Dan Bracey <penguingeek1@gmail.com>
2024-04-01 16:58:25 +0100

    Create form request

---
+++ app/Http/Requests/TimeslotRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TimeslotRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //Handled via route middleware
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'room_id' => 'string|exists:rooms,id',
+            'day_of_week' => 'numeric|min:1,max:5',
+            'start_time' => 'date_format:H:i',
+            'end_time' => 'date_format:H:i|after:time_start',
+            'is_lecture' => 'boolean'
+        ];
+    }
+}
